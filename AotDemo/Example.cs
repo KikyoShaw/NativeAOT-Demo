898c5d263c8a27475cdf8e091525bb0d468d8566
shaw <1939484341@qq.com>
2024-11-29 10:45:52 +0800

    测试demo

---
+++ AotDemo/Example.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AotDemo
+{
+    public class Example
+    {
+        [UnmanagedCallersOnly(EntryPoint = "Add")]
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
